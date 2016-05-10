2786f811eba73122ee46722514cd1728dacdcffb
shane adasm <adamsch1@yahoo.com>
2016-05-10 00:28:55 -0700

    move to qsort faster

--- co.c
+++ co.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "klib/kvec.h"
 #include "klib/khash.h"
@@ -11,21 +12,22 @@ typedef struct {
 } dt;
 
 typedef struct {
-	uint64_t  term;
 	kvec_t(uint64_t) data;
 } entry_t;
 
 KHASH_MAP_INIT_INT(m32, entry_t);
+KSORT_INIT_GENERIC( uint64_t );
 
 typedef struct {
 
+	int size;
 	khash_t(m32) * __restrict h;
 
 } sorter_writer_t;
 
-
 int sorter_init( sorter_writer_t *sw ) {
 	sw->h = kh_init(m32);
+	sw->size = 0;
 }
 
 int sorter_get( sorter_writer_t * sw, dt * pair ) {
@@ -35,25 +37,29 @@ int sorter_get( sorter_writer_t * sw, dt * pair ) {
   if( (k=kh_get(m32, sw->h, pair->term)) == kh_end( sw->h ) ) {
 		k = kh_put(m32, sw->h, pair->term, &absent );
 		kv_init( kh_value(sw->h,k).data  );
+		sw->size++;
 	}
 	kv_push( uint64_t, kh_value(sw->h, k).data, pair->doc );
 }
 
-KSORT_INIT_GENERIC( uint64_t );
+int intcmp( const void *a, const void *b ) {
+	return (uint64_t*)a-(uint64_t*)b;
+}
 
 int sorter_sort( sorter_writer_t *sw ) {
-	kvec_t(uint64_t) terms;
-	kv_init(terms);
 
+	uint64_t *terms = malloc( sizeof(uint64_t) * sw->size );
+
+	int j=0;
 	for( int k=kh_begin(sw->h); k != kh_end(sw->h); ++k ) {
 		if( kh_exist( sw->h, k) ) {
-			kv_push( uint64_t, terms, kh_key( sw->h, k));
+      terms[j++] = kh_key( sw->h, k );
 		}
 	}
 
-	ks_introsort( uint64_t,  kv_size(terms), &kv_A(terms,0));
+	qsort( terms, sw->size, sizeof(uint64_t), intcmp );
 
-  kv_destroy( terms ); 
+	free(terms);
 }
 
 int main() {
@@ -68,7 +74,7 @@ int main() {
 	  sorter_get( &sw, &pair );
 
 	for( int k=0; k<10000000; k++ ) {
-		pair.term = k%10000;
+		pair.term = k;
 		pair.doc = k;
 	  sorter_get( &sw, &pair );
 	}
